7a9f4c1197de76d2b34b67659dc388f1e2d62e57
ruthless2skech <74581915+manesh1234@users.noreply.github.com>
2022-10-05 23:47:36 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import styles from './App.module.css';
 import MovieCard from './MovieCard';
 import { FcSearch } from 'react-icons/fc';
-import {AiFillHeart } from "react-icons/ai"
+import { AiFillHeart } from "react-icons/ai"
 const API_URL = "https://www.omdbapi.com?apikey=3482c7da";
 
 const App = () => {
@@ -23,59 +23,59 @@ const App = () => {
 
 
     return (
-      <div className={styles.app}>
-        <h1>MovieLand</h1>
-        <div className={styles.search}>
-          <input
-            type="text"
-            placeholder="search for movies"
-            onChange={(e) =>
-              setSearch({ isSearch: true, name: e.target.value })
-            }
-          />
-          <FcSearch className={styles.searchbtn} />
-        </div>
+        <div className={styles.app}>
+            <h1>MovieLand</h1>
+            <div className={styles.search}>
+                <input
+                    type="text"
+                    placeholder="search for movies"
+                    onChange={(e) =>
+                        setSearch({ isSearch: true, name: e.target.value })
+                    }
+                />
+                <FcSearch className={styles.searchbtn} />
+            </div>
 
-        {movies && movies.length > 1 ? (
-          <div className={styles.container}>
-            {movies.map((c, index) => (
-              <MovieCard key={index} movie={c} />
-            ))}
-          </div>
-        ) : (
-          search.isSearch && (
-            <h2 style={{ marginTop: "20px", color: "white" }}>
-              No results found !
-            </h2>
-          )
-        )}
+            {movies && movies.length > 1 ? (
+                <div className={styles.container}>
+                    {movies.map((c, index) => (
+                        <MovieCard key={index} movie={c} />
+                    ))}
+                </div>
+            ) : (
+                search.isSearch && (
+                    <h2 style={{ marginTop: "20px", color: "white" }}>
+                        No results found !
+                    </h2>
+                )
+            )}
 
-        <div
-          className={styles.footer}
-          style={
-            !search.isSearch ? { marginTop: "495px" } : { marginTop: "0px" }
-          }
-        >
-          <h1
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Made with&nbsp;
-            <span style={{ display: "flex" }}>
-              <AiFillHeart color="red" size="30" />
-            </span>
-            &nbsp; by &nbsp;
-            <a href="https://www.linkedin.com/in/manesh-ram/" target={"_blank"}>
-              Manesh Ram
-            </a>
-          </h1>
+            <div
+                className={styles.footer}
+                style={
+                    !search.isSearch ? { marginTop: "495px" } : { marginTop: "0px" }
+                }
+            >
+                <h1
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    Made with&nbsp;
+                    <span style={{ display: "flex" }}>
+                        <AiFillHeart color="red" size="30" />
+                    </span>
+                    &nbsp; by &nbsp;
+                    <a href="https://www.linkedin.com/in/manesh-ram/" target={"_blank"}>
+                        Manesh Ram
+                    </a>
+                </h1>
+            </div>
         </div>
-      </div>
     );
-    
+
 }
 
 export default App;
